76978d6cf677b23ea1973c257a63468421841c47
sutha06 <suthakaran.siva0601@gmail.com>
2024-11-25 16:12:55 -0500

    question controller made

---
+++ Forum/Controllers/QuestionController.cs
@@ -0,0 +1,117 @@
+using Microsoft.AspNetCore.Mvc;
+using Forum.Data;
+using Forum.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Forum.Controllers;
+
+public class QuestionController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public QuestionController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Questions
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Questions.ToListAsync());
+    }
+
+    // GET: Questions/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var question = await _context.Questions
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (question == null) return NotFound();
+
+        return View(question);
+    }
+
+    // GET: Questions/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Questions/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Question question)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(question);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(question);
+    }
+
+    // GET: Questions/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var question = await _context.Questions.FindAsync(id);
+        if (question == null) return NotFound();
+        return View(question);
+    }
+
+    // POST: Questions/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Question question)
+    {
+        if (id != question.Id) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(question);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionExists(question.Id)) return NotFound();
+                else throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(question);
+    }
+
+    // GET: Questions/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var question = await _context.Questions
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (question == null) return NotFound();
+
+        return View(question);
+    }
+
+    // POST: Questions/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var question = await _context.Questions.FindAsync(id);
+        _context.Questions.Remove(question);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool QuestionExists(int id)
+    {
+        return _context.Questions.Any(e => e.Id == id);
+    }
+}
