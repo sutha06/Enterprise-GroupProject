7cb259d941d9f20e8216a43d5d02b0da3b098f7e
sutha06 <suthakaran.siva0601@gmail.com>
2024-11-25 20:34:51 -0500

    QuestionController.cs updated after bug

--- Forum/Controllers/QuestionController.cs
+++ Forum/Controllers/QuestionController.cs
@@ -3,6 +3,8 @@ using Forum.Data;
 using Forum.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Forum.Controllers;
 
@@ -25,16 +27,26 @@ public class QuestionController : Controller
     // GET: Questions/Details/5
     public async Task<IActionResult> Details(int? id)
     {
-        if (id == null) return NotFound();
+        if (id == null || _context.Questions == null)
+        {
+            return NotFound();
+        }
 
         var question = await _context.Questions
-                .Include(a=>a.Answers)
-                .ThenInclude(q=> q.User)
+            .Include(q => q.User)         // Include User
+            .Include(q => q.Answers)      // Include Answers
+            .ThenInclude(a => a.User)     // Include User for each Answer
             .FirstOrDefaultAsync(m => m.Id == id);
-        if (question == null) return NotFound();
+
+        if (question == null)
+        {
+            return NotFound();
+        }
 
         return View(question);
     }
+
+
 
     // GET: Questions/Create
     public IActionResult Create()
@@ -45,7 +57,7 @@ public class QuestionController : Controller
     // POST: Questions/Create
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create(Question question)
+    public async Task<IActionResult> Create([Bind("Id,Title,Description,IdentityUserId")] Question question)
     {
         if (ModelState.IsValid)
         {
@@ -53,8 +65,37 @@ public class QuestionController : Controller
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+            
         return View(question);
     }
+    
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> AddAnswer([Bind("Id,Content,QuestionId,IdentityUserId")] Answer answer)
+    {
+        if (ModelState.IsValid)
+        {
+            // Add answer to the database
+            _context.Add(answer);
+            await _context.SaveChangesAsync();
+        }
+
+        // Fetch the question again to include updated answers
+        var question = await _context.Questions
+            .Include(q => q.User)
+            .Include(q => q.Answers)
+            .ThenInclude(a => a.User)
+            .FirstOrDefaultAsync(q => q.Id == answer.QuestionId);
+
+        if (question == null)
+        {
+            return NotFound();
+        }
+
+        return View("Details", question);
+    }
+
 
     // GET: Questions/Edit/5
     public async Task<IActionResult> Edit(int? id)
