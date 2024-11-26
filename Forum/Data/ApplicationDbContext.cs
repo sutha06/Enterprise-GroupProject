3089b670a8113c5fb9d206f044372c5762ea27f6
PeterKorgol <114254886+PeterKorgol@users.noreply.github.com>
2024-11-26 18:53:55 -0500

    fixed delete function need to add cascade delete to datavase context

--- Forum/Data/ApplicationDbContext.cs
+++ Forum/Data/ApplicationDbContext.cs
@@ -10,7 +10,19 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
-    
+
     public DbSet<Question> Questions { get; set; }
     public DbSet<Answer> Answers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure cascade delete for Question -> Answers relationship
+        modelBuilder.Entity<Answer>()
+            .HasOne(a => a.Question)
+            .WithMany(q => q.Answers)
+            .HasForeignKey(a => a.QuestionId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
