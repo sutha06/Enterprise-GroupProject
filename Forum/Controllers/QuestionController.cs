8d56aaf4f0816a8f293be5916e4d76468867c05f
PeterKorgol <114254886+PeterKorgol@users.noreply.github.com>
2024-11-26 17:55:25 -0500

    added edit and delte buttons edit works fine but delte is giving 400 error after hitting ok

--- Forum/Controllers/QuestionController.cs
+++ Forum/Controllers/QuestionController.cs
@@ -7,162 +7,165 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
-namespace Forum.Controllers;
-
-public class QuestionController : Controller
+namespace Forum.Controllers
 {
-    private readonly ApplicationDbContext _context;
-
-    public QuestionController(ApplicationDbContext context)
+    public class QuestionController : Controller
     {
-        _context = context;
-    }
+        private readonly ApplicationDbContext _context;
 
-    // GET: Questions
-    public async Task<IActionResult> Index()
-    {
-        var applicationDbContext = _context.Questions.Include(q => q.User).Include(a=>a.Answers);
-        return View(await applicationDbContext.ToListAsync());
-    }
-
-    // GET: Questions/Details/5
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null || _context.Questions == null)
+        public QuestionController(ApplicationDbContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        var question = await _context.Questions
-            .Include(q => q.User)         
-            .Include(q => q.Answers)      
-            .ThenInclude(a => a.User)     
-            .FirstOrDefaultAsync(m => m.Id == id);
-
-        if (question == null)
+        // GET: Questions
+        public async Task<IActionResult> Index()
         {
-            return NotFound();
+            var applicationDbContext = _context.Questions.Include(q => q.User).Include(a => a.Answers);
+            return View(await applicationDbContext.ToListAsync());
         }
 
-        return View(question);
-    }
-
-
-
-    // GET: Questions/Create
-    [Authorize]
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: Questions/Create
-    [HttpPost]
-    [Authorize]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Title,Description,IdentityUserId")] Question question)
-    {
-        if (ModelState.IsValid)
+        // GET: Questions/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            _context.Add(question);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-            
-        return View(question);
-    }
-    
-    
-    [HttpPost]
-    [Authorize]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> AddAnswer([Bind("Id,Content,QuestionId,IdentityUserId")] Answer answer)
-    {
-        if (ModelState.IsValid)
-        {
-            // Add answer to the database
-            _context.Add(answer);
-            await _context.SaveChangesAsync();
-        }
-
-        // Fetch the question again to include updated answers
-        var question = await _context.Questions
-            .Include(q => q.User)
-            .Include(q => q.Answers)
-            .ThenInclude(a => a.User)
-            .FirstOrDefaultAsync(q => q.Id == answer.QuestionId);
-
-        if (question == null)
-        {
-            return NotFound();
-        }
-
-        return View("Details", question);
-    }
-
-
-    // GET: Questions/Edit/5
-    [Authorize]
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null) return NotFound();
-
-        var question = await _context.Questions.FindAsync(id);
-        if (question.IdentityUserId != User.FindFirstValue(ClaimTypes.NameIdentifier)) return NotFound();
-        return View(question);
-    }
-
-    // POST: Questions/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, Question question)
-    {
-        if (id != question.Id) return NotFound();
-
-        if (ModelState.IsValid)
-        {
-            try
+            if (id == null || _context.Questions == null)
             {
-                _context.Update(question);
+                return NotFound();
+            }
+
+            var question = await _context.Questions
+                .Include(q => q.User)
+                .Include(q => q.Answers)
+                .ThenInclude(a => a.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
+        // GET: Questions/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Questions/Create
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,IdentityUserId")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(question);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(question);
+        }
+
+        // POST: Questions/AddAnswer
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddAnswer([Bind("Id,Content,QuestionId,IdentityUserId")] Answer answer)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add answer to the database
+                _context.Add(answer);
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+
+            // Fetch the question again to include updated answers
+            var question = await _context.Questions
+                .Include(q => q.User)
+                .Include(q => q.Answers)
+                .ThenInclude(a => a.User)
+                .FirstOrDefaultAsync(q => q.Id == answer.QuestionId);
+
+            if (question == null)
             {
-                if (!QuestionExists(question.Id)) return NotFound();
-                else throw;
+                return NotFound();
             }
+
+            return View("Details", question);
+        }
+
+        // GET: Questions/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var question = await _context.Questions.FindAsync(id);
+            if (question.IdentityUserId != User.FindFirstValue(ClaimTypes.NameIdentifier)) return NotFound();
+            return View(question);
+        }
+
+        // POST: Questions/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Question question)
+        {
+            if (id != question.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(question);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuestionExists(question.Id)) return NotFound();
+                    else throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(question);
+        }
+
+        // GET: Questions/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var question = await _context.Questions
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (question == null || question.IdentityUserId != User.FindFirstValue(ClaimTypes.NameIdentifier)) return NotFound();
+
+            return View(question);
+        }
+
+        // POST: Questions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+            if (question != null)
+            {
+                _context.Questions.Remove(question);
+                await _context.SaveChangesAsync();
+            }
+
             return RedirectToAction(nameof(Index));
         }
-        return View(question);
-    }
 
-    // GET: Questions/Delete/5
-    [Authorize]
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null) return NotFound();
-
-        var question = await _context.Questions
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (question.IdentityUserId != User.FindFirstValue(ClaimTypes.NameIdentifier)) return NotFound();
-
-        return View(question);
-    }
-
-    // POST: Questions/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    [Authorize]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        var question = await _context.Questions.FindAsync(id);
-        _context.Questions.Remove(question);
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-    }
-
-    private bool QuestionExists(int id)
-    {
-        return _context.Questions.Any(e => e.Id == id);
+        private bool QuestionExists(int id)
+        {
+            return _context.Questions.Any(e => e.Id == id);
+        }
     }
 }
